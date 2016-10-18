442e565b969d267561d9310a61536c520632c1f4
David Clark <david.j.clark@avanade.com>
2016-10-18 12:22:13 -0700

    adding migrations, added passing stats table

---
+++ CodeCookingwithData/Migrations/201610181913329_PassingStatsTable.cs
@@ -0,0 +1,45 @@
+namespace CodeCookingwithData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PassingStatsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PassingStatsModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        PlayerId = c.String(),
+                        Name = c.String(),
+                        Age = c.Int(nullable: false),
+                        DateTime = c.DateTime(nullable: false),
+                        Team = c.String(),
+                        IsAway = c.Boolean(nullable: false),
+                        OppTeam = c.String(),
+                        Result = c.String(),
+                        GameNum = c.Int(nullable: false),
+                        Week = c.Int(nullable: false),
+                        DayOfWeek = c.Int(nullable: false),
+                        Completions = c.Int(nullable: false),
+                        Attempts = c.Int(nullable: false),
+                        CmpPercent = c.Int(nullable: false),
+                        Yds = c.Int(nullable: false),
+                        Tds = c.Int(nullable: false),
+                        Interceptions = c.Int(nullable: false),
+                        Rating = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        YdsperAtt = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        AdjYdsperAtt = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PassingStatsModels");
+        }
+    }
+}
---
+++ CodeCookingwithData/Models/PassingStatsModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeCookingwithData.Models
+{
+    public class PassingStatsModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PlayerId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Team { get; set; }
+        public bool IsAway { get; set; }
+        public string OppTeam { get; set; }
+        public string Result { get; set; }
+        public int GameNum { get; set; }
+        public int Week { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public int Completions { get; set; }
+        public int Attempts { get; set; }
+        public int CmpPercent { get; set; }
+        public int Yds { get; set; }
+        public int Tds { get; set; }
+        public int Interceptions { get; set; }
+        public decimal Rating { get; set; }
+        public decimal YdsperAtt { get; set; }
+        public decimal AdjYdsperAtt { get; set; }
+    }
+}
